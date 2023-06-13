8386b7cc8f9612100e64249369d72be9961e9f93
ommani-rai <132205364+ommani-rai@users.noreply.github.com>
2023-06-13 14:20:59 +0545

    operator

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+// conditionals in javascript
+/*
+1. if
+2. if else
+3. if, else if, else
+4. switch
+
+1. if
+syntax:
+if(condition){
+    // body of truth statement
+}
+
+
+*/
+var a = 1;
+if (a > 10) { // 1 > 10
+    console.log(a + "is greater than 10")
+}
