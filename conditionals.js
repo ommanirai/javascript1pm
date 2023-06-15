6675ad13d96d74e118d1b0b43406f88d4f9ab067
ommani-rai <132205364+ommani-rai@users.noreply.github.com>
2023-06-15 14:48:09 +0545

    if and if else

--- conditionals.js
+++ conditionals.js
@@ -11,9 +11,63 @@ if(condition){
     // body of truth statement
 }
 
+condition:
+truthy value: true, 1, 'ram' 
+falsy value: false,0, '', undefined, NaN
+
+2. if else
+syntax:
+if(condition){
+    // truth statement
+}
+else {
+    // false statement
+}
 
 */
-var a = 1;
-if (a > 10) { // 1 > 10
-    console.log(a + "is greater than 10")
+
+// var a = 12;
+// if ('') {
+//     console.log("true block goes here")
+// }
+
+// console.log(a < 20)
+var english = 59
+
+
+var number = 100
+if (number > 500) { // number < 500 => 100 < 500
+    console.log("number is less than 500")
+}
+else {
+    console.log("number is greater than 500")
+}
+
+var nepali = 50;
+var english = 38;
+var math = 80;
+var science = 80;
+var health = 67;
+if (nepali >= 35 && english >= 35 && math >= 35 && science >= 35 && health >= 35) {
+    console.log("congratulation")
+    var per = ((nepali + english + math + science + health) / 500) * 100
+    if (per >= 80) {
+        console.log("you got distinction")
+        console.log("your percentage is: ", per)
+    }
+    if (per < 80 && per >= 60) {
+        console.log("you got first division")
+        console.log("your percentage is: ", per)
+    }
+    if (per < 60 && per >= 45) {
+        console.log("you got second division")
+        console.log("your percentage is: ", per)
+    }
+    if (per < 45) {
+        console.log("my boy, you got third division")
+        console.log("your percentage is: ", per)
+    }
+}
+else {
+    console.log("try again")
 }
