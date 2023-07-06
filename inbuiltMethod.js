e72a94b9b18a74a333342261a63ce4731737b46f
ommani-rai <132205364+ommani-rai@users.noreply.github.com>
2023-07-06 14:00:04 +0545

    digital clock

---
+++ inbuiltMethod.js
@@ -0,0 +1,31 @@
+/*
+js methods
+1. setInterval()
+2. setTimeout()
+3. clearInterval()
+4. typeof()
+Number()
+String()
+
+type conversion/coercion
+1. internal conversion(internal coercion)
+2. external conversion(external coercion)
+
+*/
+// var name = '4'
+// // console.log(typeof(Number(name)))
+// var num = 3;
+// console.log(name * num)
+
+// inbuilt method
+// string
+var college = "United"
+console.log(college.substring(1, 2))
+console.log(college.substr(1, 2))
+// console.log(college.length)
+// console.log(college.toUpperCase())
+// console.log(college.toLowerCase())
+// console.log(college.trim().length)
+
+
+
