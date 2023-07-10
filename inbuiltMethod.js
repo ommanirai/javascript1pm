e6db4da51f787d85f690ea5abe25e55b981aab04
ommani-rai <132205364+ommani-rai@users.noreply.github.com>
2023-07-10 14:10:21 +0545

    inbuilt method for object and number

--- inbuiltMethod.js
+++ inbuiltMethod.js
@@ -20,12 +20,54 @@ type conversion/coercion
 // inbuilt method
 // string
 var college = "United"
-console.log(college.substring(1, 2))
-console.log(college.substr(1, 2))
+// console.log(college.substring(1, 2))
+// console.log(college.substr(1, 2))
 // console.log(college.length)
 // console.log(college.toUpperCase())
 // console.log(college.toLowerCase())
 // console.log(college.trim().length)
 
+// Number
+var num = '12.987987'
+// console.log(isNaN('1212sdfsd'))
+// console.log(num.toFixed(0))
+// console.log(parseInt(num))
+// console.log(num)
+// console.log(parseFloat(num))
 
 
+// includes
+var email = "ommanirai27@gmail.com"
+
+// console.log(email.includes(".com@gmail"))
+// if (email.includes("@") && email.includes("gmail") && email.includes(".com")) {
+//     console.log("valid email")
+// }
+// else {
+//     console.log("invalid email")
+// }
+
+// Object
+var phone = {
+    brand: 'samsung',
+    price: 13000,
+    ram: '4gb',
+}
+console.log(typeof(phone))
+// object to string
+var newPhone = JSON.stringify(phone)
+console.log(newPhone)
+console.log(typeof(newPhone))
+
+// string to object
+var oldPhone = JSON.parse(newPhone)
+console.log(oldPhone)
+console.log(typeof(oldPhone))
+
+
+// console.log(phone.brand)
+// console.log(phone['brand'])
+
+// console.log(phone.hasOwnProperty("brand"))
+// console.log(Object.keys(phone))
+// console.log(Object.values(phone))
